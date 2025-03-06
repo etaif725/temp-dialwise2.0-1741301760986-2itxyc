40d31b618e4499a816cb6db239f1d2ae4a7cc74e
ETAI FISHLER <142711245+etaif725@users.noreply.github.com>
2025-03-06 16:56:30 -0600

    Copy app/privacy/page.tsx from dialwise2.0

---
+++ app/privacy/page.tsx
@@ -0,0 +1,79 @@
+import { Metadata } from "next";
+import DWPrivacy from "./page-csr";
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://dialwise.ai/privacy'),
+  title: {
+    default: 'Our AI Agents Solutions for Businesses | DialWise.ai',
+    template: '%s | DialWise.ai',
+  },
+  description:
+    'DialWise.ai offers a range of AI voice agents and chatbot automation solutions for businesses to improve customer service and increase revenue.',
+  keywords: [
+    'AI voice agents',
+    'conversational AI',
+    'customer service automation',
+    'AI chatbots',
+    'virtual assistants',
+    '24/7 customer support',
+    'voice AI',
+    'customer experience',
+    'business automation',
+    'affiliates',
+    'affiliates program',
+    'dialwise affiliates',
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: '/',
+    title: 'Our AI Agents Solutions for Businesses | DialWise.ai',
+    description:
+      'DialWise.ai offers a range of AI voice agents and chatbot automation solutions for businesses to improve customer service and increase revenue.',
+    siteName: 'DialWise.ai',
+    images: [
+      {
+        url: '/demo_dialwise.webp',
+        width: 1200,
+        height: 630,
+        alt: 'DialWise.ai - Revolutionary AI Voice Agents & Chatbots',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@dialwise',
+    creator: '@dialwise',
+    title: 'Our AI Agents Solutions for Businesses | DialWise.ai',
+    description:
+      'DialWise.ai offers a range of AI voice agents and chatbot automation solutions for businesses to improve customer service and increase revenue.',
+    images: ['/demo_dialwise.webp'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+      'max-video-preview': -1,
+    },
+  },
+  alternates: {
+    languages: {
+      'en-US': '/',
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    shortcut: '/shortcut-icon.png',
+  },
+};
+
+export default function Home() {
+  return (
+    <DWPrivacy />
+  );
+}
